15dbc4145da08e4cea127d80f17c221d1cb33d9c
mygit@23 <naveenmadhesan23@gmail.com>
2024-01-02 14:49:55 +0530

    Add project files.

---
+++ Jan2ClassLibrary/College.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jan2ClassLibrary
+{
+    public class College
+    {
+        public void Ece()
+        {
+            Console.WriteLine("Available ECE Couse");
+        }
+        public void Eee() {
+            Console.WriteLine("Available EEE Course");
+        }
+        public void Mech() { Console.WriteLine("Available Mechanical Course"); }
+    }
+}
+// D:\Mphasis\SimpleliLearn Doc .net\2jan\Jan2ClassLibrary\Jan2ClassLibrary\bin\Debug\Jan2ClassLibrary.dll
